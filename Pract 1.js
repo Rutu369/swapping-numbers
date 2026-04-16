697871a2a0b4108f786cd697b81c75fe9fba0154
Ruturaj Kundekar <ruturajkundekar77@gmail.com>
2026-04-16 10:25:20 +0530

    Update Pract 1.js

--- Pract 1.js
+++ Pract 1.js
@@ -1,95 +1,28 @@
-1) Implement Student Course Registration with the following details using Node.js 
+A school wants to automate the process of calculating student results to avoid manual work and errors. In this system, a student enters marks for three subjects, each typically out of 100. The program then calculates the total marks by adding all three subject scores and computes the average by dividing the total by three. Based on this average, the program assigns a grade according to predefined criteria: if the average is 90 or above, the student gets Grade A; if it is between 60 and 89, the student gets Grade B; otherwise, the student is considered to have failed. This system helps teachers quickly generate accurate results, reduces calculation time, and ensures consistency in grading, making it useful for real-life school result processing. Write a Java program to calculate the total marks, average, and grade of a student based on marks of three subjects.
 
-•Create a file for all the courses with course name, course code, LTP model and 
+ import java.util.Scanner;
+class StudentMarks
+{
+        public static void main(String[] args)
+       {
+        Scanner sc = new Scanner(System.in);
+        int m1, m2, m3, total;
+        double avg;
+        System.out.print("Enter marks of 3 subjects: ");
+        m1 = sc.nextInt();
+        m2 = sc.nextInt();
+        m3 = sc.nextInt();
 
-credits. 
+        total = m1 + m2 + m3;
+        avg = total / 3.0;
+        System.out.println("Total = " + total);
+        System.out.println("Average = " + avg);
 
-•Display the file content on the web browser using the HTTP module. 
-
-const http = require('http'); 
-
-const fs = require('fs'); 
-
-var course_list = [ 
-
-{ 'si_no': 1, 'course_code': '23EBCC202', 'course_name': 'Java', 'ltp_model': '4-0-0', 'credits': 4 
-
-}, 
-
-{ 'si_no': 2, 'course_code': '24EBCC201', 'course_name': 'Full Stack Web Development with 
-
-MERN Lab', 'ltp_model': '0-0-2', 'credits': 2 }, 
-
-{ 'si_no': 3, 'course_code': '22EBCP102', 'course_name': 'Software Testing', 'ltp_model': '3-0-
-
-0', 'credits': 3 }, 
-
-{ 'si_no': 4, 'course_code': '23EBCC201', 'course_name': 'Computer Networks', 'ltp_model': 
-
-'4-0-0', 'credits': 4 }, 
-
-{ 'si_no': 5, 'course_code': '22EBCP201', 'course_name': 'Machine Learning', 'ltp_model': '4-
-
-0-0', 'credits': 4}, 
-
-{ 'si_no': 6, 'course_code': '23EBCC202', 'course_name': 'Java Lab', 'ltp_model': '0-0-1.5', 
-
-'credits': 1.5 } 
-
-]; 
-
-var file_name = 'course_info.txt'; 
-
-if (fs.existsSync(file_name)) { 
-
-fs.unlinkSync(file_name); 
-
-  }
-fs.writeFileSync(file_name, ''); 
-
-for (var course_element = 0; course_element < course_list.length; course_element++) { 
-
-let si_no = course_list[course_element]['si_no'] + '\t| '; 
-
-let course_code = course_list[course_element]['course_code'] + '\t| '; 
-
-let course_name = course_list[course_element]['course_name'] + '\t| '; 
-
-let ltp_model = course_list[course_element]['ltp_model'] + '\t| '; 
-
-let credits = course_list[course_element]['credits'] + '\t| '; 
-
-var course_details = si_no + course_code + course_name + ltp_model + credits + '\n'; 
-
-fs.appendFileSync(file_name, course_details); 
-
-} 
-
-http.createServer((request, response) => { 
-
-response.writeHead(200, { 'Content-Type': 'text/html' }); 
-
-response.write('<h1 style="text-align:center">Course Information</h1>'); 
-
-let si_header = '<table border="1" style="text-align:center"><tr><th>SI No</th><th>Course 
-
-Code</th><th>Course Name</th><th>LTP Model</th><th>Credits</th></tr>'; 
-
-let table_content = ''; 
-
-course_list.forEach((course) => { 
-
-table_content += '<tr><td>' + course.si_no + '</td><td>' + course.course_code + '</td><td>' 
-
-+ course.course_name + '</td><td>' + course.ltp_model + '</td><td>' + course.credits + 
-
-'</td></tr>';
-  }); 
-
-response.write(si_header + table_content + '</table>'); 
-
-response.end(); 
-
-}).listen(2001); 
-
-console.log("Server started on port 2001");
+        if (avg >= 90)
+            System.out.println("Grade A");
+        else if (avg >= 60)
+            System.out.println("Grade B");
+        else
+            System.out.println("Fail");
+    }
+}
